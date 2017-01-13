440d31badda4ab8cfdf4c1b2037942e6eb383c38
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-01-13 12:55:56 +0100

    Fixed bug lookButton

--- Adventure/Adventure/app/src/main/java/com/cazallau/adventure/MainActivity.java
+++ Adventure/Adventure/app/src/main/java/com/cazallau/adventure/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                mainText.setText(currentRoom.print());
+                mainText.setText(currentRoom.getDescription() + "\n" + currentRoom.print());
             }
         });
 
