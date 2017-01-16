9cfba2dfa01285c4d1024058bf837df72a116b20
Antonio Jesus Cazalla <gemabeltran@MacBook-Air-de-Gema.local>
2017-01-16 09:01:26 +0100

    added rooms

--- Adventure/Adventure/app/src/main/java/com/cazallau/adventure/Model/MaoGenerator.java
+++ Adventure/Adventure/app/src/main/java/com/cazallau/adventure/Model/MaoGenerator.java
@@ -21,11 +21,18 @@ public class MaoGenerator {
         room3.setDescription("Hay una barra de bar con tapiceria roja pasada de moda. Huele a tabaco");
         room3.setImage(R.drawable.room3);
 
+        Room room4 = new Room();
+        room4.setDescription("Un timon de barco se encuentra en medio de la habitacion rodeado por un charco de sangre");
+
+
+
         //poner a cada habitacion que tiene al lado
         room1.setRoomSouth(room2);
         room2.setRoomNorth(room1);
         room2.setRoomEast(room3);
         room3.setRoomWest(room2);
+        room4.setRoomWest(room1);
+        room1.setRoomEast(room4);
 
         //creamos la lista de items y se la pasamos a a room3
         LinkedList<Item> itemsRoom3 = new LinkedList<>();
